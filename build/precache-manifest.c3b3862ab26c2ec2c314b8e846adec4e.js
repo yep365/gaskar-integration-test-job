45eb3f84f7687c4ce3c49024bb8ac18234bc232d
yep365 <kacherov01@gmail.com>
2020-08-09 14:52:53 +0300

    Build added.

---
+++ build/precache-manifest.c3b3862ab26c2ec2c314b8e846adec4e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25009683277f08616f152cf615ae4d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bab8a6dbffada7f1707",
+    "url": "/static/css/2.9d15067e.chunk.css"
+  },
+  {
+    "revision": "0fd2f149b3e131c6a2b3",
+    "url": "/static/css/main.b8bfd162.chunk.css"
+  },
+  {
+    "revision": "3bab8a6dbffada7f1707",
+    "url": "/static/js/2.2cbc5c15.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.2cbc5c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd2f149b3e131c6a2b3",
+    "url": "/static/js/main.6d696095.chunk.js"
+  },
+  {
+    "revision": "1f9f3f822fc1b248220e",
+    "url": "/static/js/runtime-main.4fcc7e2c.js"
+  },
+  {
+    "revision": "989411b156000354a808ad0b184bdca6",
+    "url": "/static/media/Logo.989411b1.png"
+  },
+  {
+    "revision": "773fdb213f1e301ce699128130a6c0cb",
+    "url": "/static/media/arrow.773fdb21.svg"
+  }
+]);
